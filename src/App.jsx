373ada69f9bdff9c903cf805965cc6eb7e54151a
suman <imrancodingschool@gmail.com>
2024-09-18 10:31:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Quiz from './Quiz';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='app-title'>MCQ Quiz Game</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
